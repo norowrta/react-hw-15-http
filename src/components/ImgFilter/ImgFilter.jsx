02d247bb93b8727cc0d7e420c3b9dd88f812d655
norowrta <m.bohatovv@gmail.com>
2026-02-22 23:35:47 +0100

    feat: add modal, load more, localStorage save, enter key

--- src/components/ImgFilter/ImgFilter.jsx
+++ src/components/ImgFilter/ImgFilter.jsx
@@ -1,30 +1,58 @@
 import css from "./ImgFilter.module.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { IoSearchOutline } from "react-icons/io5";
 
-// apiKey не обов'язково імпортувати з .env, але зазвичай  api ключі  записуються в .env, щоб ніхто не міг їх побачити
 const apiKey = import.meta.env.VITE_API_KEY;
 
 import axios from "axios";
 
 export default function ImgFilter() {
-  const [searchQuery, setSearchQuery] = useState("");
+  const [searchQuery, setSearchQuery] = useState(() => {
+    const savedQuery = localStorage.getItem("searchQuery");
+    return savedQuery ? savedQuery : "";
+  });
+  const [query, setQuery] = useState(searchQuery);
   const [images, setImages] = useState([]);
+  const [modalImage, setModalImage] = useState(null);
+  const [isLoaded, setLoaded] = useState(false);
+  const [page, setPage] = useState(1);
 
-  const getImages = async () => {
-    try {
-      const respons = await axios.get("https://pixabay.com/api/", {
-        params: {
-          q: searchQuery,
-          // q це шота тіпа параметр pixabay, тоїсть те, що йде після / в кінці http запиту, а searchQuery це значення q (тоїсть змінна)
-          key: apiKey,
-        },
-      });
-      setImages(respons.data.hits);
+  useEffect(() => {
+    if (query === "") {
+      return;
+    }
 
-      // data.hits це шлях до картинок, якщо ти пропишеш просто respons в консоль, то там буде великий json файл з іншими об'єктами і масивами всередині
-    } catch (error) {
-      console.log(error);
+    const fetchImages = async () => {
+      try {
+        const respons = await axios.get("https://pixabay.com/api/", {
+          params: {
+            q: query,
+            key: apiKey,
+            page,
+            per_page: 12,
+          },
+        });
+
+        if (page === 1) {
+          setImages(respons.data.hits);
+        } else {
+          setImages((prevImages) => [...prevImages, ...respons.data.hits]);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchImages();
+  }, [query, page]);
+
+  useEffect(() => {
+    localStorage.setItem("searchQuery", searchQuery);
+  }, [searchQuery]);
+
+  const handleKeyDown = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit();
     }
   };
 
@@ -33,11 +61,38 @@ export default function ImgFilter() {
   }
 
   function handleSubmit() {
-    getImages();
+    setQuery(searchQuery);
+    setPage(1);
+  }
+
+  function handleModal(event) {
+    if (event.target.tagName !== "IMG") {
+      return;
+    }
+
+    setLoaded(false);
+
+    setModalImage({
+      src: event.target.src,
+      alt: event.target.alt,
+    });
+  }
+
+  function handleLoad() {
+    if (isLoaded === false) {
+      setLoaded(true);
+    } else if (isLoaded === true) {
+      return;
+    }
+  }
+
+  function closeModal() {
+    setModalImage(null);
+    setLoaded(false);
   }
 
   return (
-    <section>
+    <section onKeyDown={handleKeyDown} className={css.section}>
       <div className={css.searchBar}>
         <button className={css.searchButton} onClick={handleSubmit}>
           <IoSearchOutline />
@@ -51,7 +106,7 @@ export default function ImgFilter() {
         />
       </div>
       <div className={css.container}>
-        <div className={css.content}>
+        <div className={css.content} onClick={handleModal}>
           {images.map((image) => {
             return (
               <img
@@ -63,7 +118,34 @@ export default function ImgFilter() {
             );
           })}
         </div>
+
+        {images.length > 0 && (
+          <div className={css.loader}>
+            <button
+              className={css.loadMoreBtn}
+              onClick={() => setPage((prevPage) => prevPage + 1)}
+            >
+              Load more
+            </button>
+          </div>
+        )}
       </div>
+
+      {modalImage && (
+        <div className={css.modal} onClick={closeModal}>
+          <div
+            className={css.modalContent}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <img
+              src={modalImage.src}
+              alt={modalImage.alt}
+              className={`${css.handleImg} ${isLoaded ? css.loaded : ""} `}
+              onLoad={handleLoad}
+            />
+          </div>
+        </div>
+      )}
     </section>
   );
 }
